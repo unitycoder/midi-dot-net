53c369a55bb6aa0d212caed2e1d04824fb1894a6
tdl <tdl@monkeyspeak.com>
2009-11-27 00:29:31 +0000

    Generalized example program to support multiple examples.

---
+++ MidiExamples/DetectDevices.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) 2009, Tom Lokovic
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Midi;
+
+namespace MidiExamples
+{
+    class DetectDevices : ExampleBase
+    {
+        public DetectDevices()
+            : base("DetectDevices.cs", "Prints info about devices.")
+        { }
+
+        public override void Run()
+        {
+            if (MidiInputDevice.InstalledDevices.Count == 0)
+            {
+                Console.WriteLine("No input devices.");
+            }
+            else
+            {
+                Console.WriteLine("Input Devices:");
+                foreach (MidiInputDevice device in MidiInputDevice.InstalledDevices)
+                {
+                    Console.WriteLine("  {0}", device.Name);
+                }
+            }
+            Console.WriteLine();
+
+            if (MidiOutputDevice.InstalledDevices.Count == 0)
+            {
+                Console.WriteLine("No output devices.");
+            }
+            else
+            {
+                Console.WriteLine("Output Devices:");
+                foreach (MidiOutputDevice device in MidiOutputDevice.InstalledDevices)
+                {
+                    Console.WriteLine("  {0}", device.Name);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
