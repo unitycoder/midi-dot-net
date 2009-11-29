71ef652290e02af4efbb1cb2cc8f1df8965a10fd
tdl@monkeyspeak.com <tdl@monkeyspeak.com@13de91ae-da45-11de-ae71-c59d11f48c9c>
2009-11-29 18:54:23 +0000

    Moves enum types to their own files.

---
+++ trunk/Midi/Channel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Midi
+{
+    /// <summary>
+    /// MIDI Channels.
+    /// </summary>
+    /// MIDI channels are named starting at 1, but encoded programmatically starting at 0.
+    ///
+    /// Channel10 is the dedicated percussion channel.
+    public enum Channel
+    {
+        /// <summary> MIDI Channel 1. </summary>
+        Channel1 = 0,
+        /// <summary> MIDI Channel 2. </summary>
+        Channel2 = 1,
+        /// <summary> MIDI Channel 3. </summary>
+        Channel3 = 2,
+        /// <summary> MIDI Channel 4. </summary>
+        Channel4 = 3,
+        /// <summary> MIDI Channel 5. </summary>
+        Channel5 = 4,
+        /// <summary> MIDI Channel 6. </summary>
+        Channel6 = 5,
+        /// <summary> MIDI Channel 7. </summary>
+        Channel7 = 6,
+        /// <summary> MIDI Channel 8. </summary>
+        Channel8 = 7,
+        /// <summary> MIDI Channel 9. </summary>
+        Channel9 = 8,
+        /// <summary> MIDI Channel 10, the dedicated percussion channel. </summary>
+        Channel10 = 9,
+        /// <summary> MIDI Channel 11. </summary>
+        Channel11 = 10,
+        /// <summary> MIDI Channel 12. </summary>
+        Channel12 = 11,
+        /// <summary> MIDI Channel 13. </summary>
+        Channel13 = 12,
+        /// <summary> MIDI Channel 14. </summary>
+        Channel14 = 13,
+        /// <summary> MIDI Channel 15. </summary>
+        Channel15 = 14,
+        /// <summary> MIDI Channel 16. </summary>
+        Channel16 = 15
+    };
+
+    /// <summary>
+    /// Extension methods for the Channel enum.
+    /// </summary>
+    public static class ChannelExtensionMethods
+    {
+        /// <summary>
+        /// Returns true if the specified channel is valid.
+        /// </summary>
+        /// <param name="channel">The channel to test.</param>
+        public static bool IsValid(this Channel channel)
+        {
+            return (int)channel >= 0 && (int)channel < 16;
+        }
+
+        /// <summary>
+        /// Throws an exception if channel is not valid.
+        /// </summary>
+        /// <param name="channel">The channel to validate.</param>
+        public static void Validate(this Channel channel)
+        {
+            if (!channel.IsValid())
+            {
+                throw new Exception("invalid Channel");
+            }
+        }
+
+        /// <summary>
+        /// Table of channel names.
+        /// </summary>
+        private static string[] ChannelNames = new string[]
+        {
+            "Channel 1",
+            "Channel 2",
+            "Channel 3",
+            "Channel 4",
+            "Channel 5",
+            "Channel 6",
+            "Channel 7",
+            "Channel 8",
+            "Channel 9",
+            "Channel 10"
+        };
+
+        /// <summary>
+        /// Returns the human-readable name of a MIDI channel.
+        /// </summary>
+        /// <param name="channel">The channel.</param>
+        public static string Name(this Channel channel)
+        {
+            if (!channel.IsValid())
+            {
+                throw new InvalidOperationException("invalid Channel");
+            }
+            return ChannelNames[(int)channel];
+        }
+    }
+}
---
+++ trunk/Midi/Control.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Midi
+{
+    /// <summary>
+    /// MIDI Controls, used in Control Change messages.
+    /// </summary>
+    /// This is an incomplete list of controls, and the details of how each control is encoded and
+    /// used is complicated.  See the MIDI spec for details.
+    ///
+    /// The most commonly used control is SustainPedal, which is considered off when &lt; 64, on when &gt; 64.
+    public enum Control
+    {
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        ModulationWheel = 1,
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        DataEntryMSB = 6,
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        Volume = 7,
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        Pan = 10,
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        Expression = 11,
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        DataEntryLSB = 38,
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        SustainPedal = 64,
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        ReverbLevel = 91,
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        TremoloLevel = 92,
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        ChorusLevel = 93,
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        CelesteLevel = 94,
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        PhaserLevel = 95,
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        NonRegisteredParameterLSB = 98,
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        NonRegisteredParameterMSB = 99,
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        RegisteredParameterNumberLSB = 100,
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        RegisteredParameterNumberMSB = 101,
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        AllControllersOff = 121,
+        /// <summary>General MIDI Control--See MIDI spec for details.</summary>
+        AllNotesOff = 123
+    }
+
+    /// <summary>
+    /// Extension methods for the Control enum.
+    /// </summary>
+    public static class ControlExtensionMethods
+    {
+        /// <summary>
+        /// Returns true if the specified control is valid.
+        /// </summary>
+        /// <param name="control">The Control to test.</param>
+        public static bool IsValid(this Control control)
+        {
+            return (int)control >= 0 && (int)control < 128;
+        }
+
+        /// <summary>
+        /// Throws an exception if control is not valid.
+        /// </summary>
+        /// <param name="control">The control to validate.</param>
+        public static void Validate(this Control control)
+        {
+            if (!control.IsValid())
+            {
+                throw new Exception("invalid Control");
+            }
+        }
+
+        /// <summary>
+        /// Table of control names.
+        /// </summary>
+        public static Dictionary<int, string> ControlNames = new Dictionary<int, string>
+        {
+            {1, "Modulation wheel"},
+            {6, "Data Entry MSB"},
+            {7, "Volume"},
+            {10, "Pan"},
+            {11, "Expression"},
+            {38, "Data Entry LSB"},
+            {64, "Sustain pedal"},
+            {91, "Reverb level"},
+            {92, "Tremolo level"},
+            {93, "Chorus level"},
+            {94, "Celeste level"},
+            {95, "Phaser level"},
+            {98, "Non-registered Parameter LSB"},
+            {99, "Non-registered Parameter MSB"},
+            {100, "Registered Parameter Number LSB"},
+            {101, "Registered Parameter Number MSB"},
+            {121, "All controllers off"},
+            {123, "All notes off"}
+        };
+
+        /// <summary>
+        /// Returns the human-readable name of a MIDI control.
+        /// </summary>
+        /// <param name="control">The control.</param>
+        public static string Name(this Control control)
+        {
+            if (!control.IsValid())
+            {
+                throw new InvalidOperationException("invalid Control");
+            }
+            if (ControlNames.ContainsKey((int)control))
+            {
+                return ControlNames[(int)control];
+            }
+            else
+            {
+                return "Other Control (see MIDI spec for details).";
+            }
+        }
+    }
+}
