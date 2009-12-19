bf83fb789a9bb4121351d8e12c50761391f69243
tdl <tdl@monkeyspeak.com>
2009-12-19 04:29:07 +0000

    Added the ability to parse Note and Chord from strings (eg "C/G" and "Abm"), which makes testing much easier and would also enable creation of notes and chords by text entry.
    Added unit tests for Chord.

--- MidiUnitTests/NoteTest.cs
+++ MidiUnitTests/NoteTest.cs
@@ -1,7 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// Copyright (c) 2009, Tom Lokovic
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+using System;
 using Midi;
 
 namespace MidiUnitTests
@@ -11,11 +32,23 @@ namespace MidiUnitTests
     class NoteTest
     {
         [Test]
-        public void ConstructionErrors()
+        public void Construction()
         {
             Assert.Throws(typeof(ArgumentOutOfRangeException), () => new Note('H'));
             Assert.Throws(typeof(ArgumentOutOfRangeException), () => new Note('c'));
             Assert.Throws(typeof(ArgumentOutOfRangeException), () => new Note('c', Note.Natural));
+            Assert.AreEqual(new Note('B'), new Note('B', Note.Natural));
+            Assert.AreEqual(new Note('C'), new Note("C"));
+            Assert.AreEqual(new Note('D', Note.Sharp), new Note("D#"));
+            Assert.AreEqual(new Note('E', Note.DoubleSharp), new Note("E##"));
+            Assert.AreEqual(new Note('F', Note.Flat), new Note("Fb"));
+            Assert.AreEqual(new Note('G', Note.DoubleFlat), new Note("Gbb"));
+            Assert.Throws(typeof(ArgumentNullException), () => new Note((string)null));
+            Assert.Throws(typeof(ArgumentException), () => new Note(""));
+            Assert.Throws(typeof(ArgumentException), () => new Note("X"));
+            Assert.Throws(typeof(ArgumentException), () => new Note("Cf"));
+            Assert.Throws(typeof(ArgumentException), () => new Note("C##x"));
+            Assert.Throws(typeof(ArgumentException), () => new Note("Db#"));
         }
         
         [Test]
@@ -31,7 +64,7 @@ namespace MidiUnitTests
         }
 
         [Test]
-        public void IsEnharmonicWithTest()
+        public void IsEnharmonicWith()
         {
             Assert.True(new Note('C').IsEharmonicWith(new Note('C')));
             Assert.True(new Note('B', Note.Sharp).IsEharmonicWith(new Note('C', Note.Natural)));
@@ -45,38 +78,38 @@ namespace MidiUnitTests
         }
 
         [Test]
-        public void PropertiesTest()
+        public void Properties()
         {
-            Assert.AreEqual(new Note('C').Letter, 'C');
-            Assert.AreEqual(new Note('C', Note.Sharp).Letter, 'C');
-            Assert.AreEqual(new Note('D', Note.Sharp).Letter, 'D');
-            Assert.AreEqual(new Note('D', Note.Flat).Letter, 'D');
-            Assert.AreEqual(new Note('B').Letter, 'B');
+            Assert.AreEqual(new Note("C").Letter, 'C');
+            Assert.AreEqual(new Note("C#").Letter, 'C');
+            Assert.AreEqual(new Note("D#").Letter, 'D');
+            Assert.AreEqual(new Note("Db").Letter, 'D');
+            Assert.AreEqual(new Note("B").Letter, 'B');
 
-            Assert.AreEqual(new Note('C').Accidental, Note.Natural);
-            Assert.AreEqual(new Note('C', Note.Sharp).Accidental, Note.Sharp);
-            Assert.AreEqual(new Note('D', Note.Sharp).Accidental, Note.Sharp);
-            Assert.AreEqual(new Note('D', Note.Flat).Accidental, Note.Flat);
-            Assert.AreEqual(new Note('B').Accidental, Note.Natural);
+            Assert.AreEqual(new Note("C").Accidental, Note.Natural);
+            Assert.AreEqual(new Note("C#").Accidental, Note.Sharp);
+            Assert.AreEqual(new Note("D#").Accidental, Note.Sharp);
+            Assert.AreEqual(new Note("Db").Accidental, Note.Flat);
+            Assert.AreEqual(new Note("B").Accidental, Note.Natural);
 
-            Assert.AreEqual(new Note('C').PositionInOctave, 0);
-            Assert.AreEqual(new Note('C', Note.Sharp).PositionInOctave, 1);
-            Assert.AreEqual(new Note('D', Note.Sharp).PositionInOctave, 3);
-            Assert.AreEqual(new Note('D', Note.Flat).PositionInOctave, 1);
-            Assert.AreEqual(new Note('B').PositionInOctave, 11);
-            Assert.AreEqual(new Note('B', Note.Sharp).PositionInOctave, 0);
+            Assert.AreEqual(new Note("C").PositionInOctave, 0);
+            Assert.AreEqual(new Note("C#").PositionInOctave, 1);
+            Assert.AreEqual(new Note("D#").PositionInOctave, 3);
+            Assert.AreEqual(new Note("Db").PositionInOctave, 1);
+            Assert.AreEqual(new Note("B").PositionInOctave, 11);
+            Assert.AreEqual(new Note("B#").PositionInOctave, 0);
         }
 
         [Test]
         public void ToStringTest()
         {
-            Assert.AreEqual(new Note('C').ToString(), "C");
+            Assert.AreEqual(new Note("C").ToString(), "C");
             Assert.AreEqual(new Note('F', 5).ToString(), "F#####");
             Assert.AreEqual(new Note('G', -3).ToString(), "Gbbb");
         }
 
         [Test]
-        public void NoteToPitchTest()
+        public void NoteToPitch()
         {
             Assert.AreEqual(new Note('C').PitchInOctave(4), Pitch.C4);
             Assert.AreEqual(new Note('C', Note.Sharp).PitchInOctave(2), Pitch.CSharp2);
@@ -99,7 +132,7 @@ namespace MidiUnitTests
         }
 
         [Test]
-        public void SemitonesTest()
+        public void Semitones()
         {
             Assert.AreEqual(new Note('C').SemitonesUpTo(new Note('C')), 0);
             Assert.AreEqual(new Note('C').SemitonesUpTo(new Note('C', Note.Sharp)), 1);
